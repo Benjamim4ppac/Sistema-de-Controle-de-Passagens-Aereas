387de55a965369945cd329302ebd5ea431c8f975
Pedro Henrique <pedrohpmata.si@gmail.com>
2026-06-15 15:32:30 -0300

    Create Struct Passageiro
    Criação da Struct 1 de Passageiros

--- main.c
+++ main.c
@@ -1,3 +1,13 @@
+typedef struct passageiro
+{
+    char nome[100];
+    char cpf[14];
+    char telefone[11];
+    char email[50];
+    char dataNascimento[11];
+
+};
+
 typedef struct {
     char assentos[5];
 } Passagem;
